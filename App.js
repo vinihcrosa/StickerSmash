f5291d593d4245305d61d5c14d396fa0d22e5cf4
Teddy <vinicius.rosa@gorilainvest.com.br>
2023-02-13 00:25:06 -0300

    feat: pegar foto do dispositivo

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
 import Button from './components/Button';
 import ImageViewer from './components/ImageViewer';
@@ -7,13 +9,35 @@ import ImageViewer from './components/ImageViewer';
 const PlaceholderImage = require('./assets/background-image.jpg');
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    })
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert('You did not select an image.');
+    }
+  }
+
   return (
     <View style={styles.container}>
       <View styles={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={PlaceholderImage} />
+        <ImageViewer
+          placeholderImageSource={PlaceholderImage}
+          selectedImage={selectedImage}
+        />
       </View>
       <View style={styles.footerContainer}>
-        <Button theme='primary' label='Choose a Photo' />
+        <Button
+          theme='primary'
+          label='Choose a Photo'
+          onPress={pickImageAsync}
+        />
         <Button label='Use this Photo' />
       </View>
       <StatusBar style="auto" />
